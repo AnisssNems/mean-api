7932b88342f8badcca66c1fab985e15b534d7a8f
Anis Jaouadi <anisssjaouadi@gmail.com>
2019-08-25 09:24:19 +0100

    intro model plat, restaurant

---
+++ models/plats.js
@@ -0,0 +1,10 @@
+var mongoose=require('mongoose');
+var schema=mongoose.Schema;
+
+var platSchema= new schema({
+    nom:String,
+    ingredient:String, 
+    prix:Number
+});
+var Plat= mongoose.model('Plat',platSchema);
+module.exports=Plat;
